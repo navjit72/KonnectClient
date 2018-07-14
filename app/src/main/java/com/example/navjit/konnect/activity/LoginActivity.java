da77c8df87331d4f01aab669cde42a114e082814
Navjit Kaur <n01267930@humbermail.ca>
2018-07-14 15:35:02 -0400

    Chatlist update bug fix

--- app/src/main/java/com/example/navjit/konnect/activity/LoginActivity.java
+++ app/src/main/java/com/example/navjit/konnect/activity/LoginActivity.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
+import com.google.gson.Gson;;
 
 import java.util.ArrayList;
 
